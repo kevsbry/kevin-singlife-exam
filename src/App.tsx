e2d60d38ff75e2b6fadad74d4500117efa53cc94
kevsbry <nivekbryan09.projects@gmail.com>
2022-07-17 17:00:12 +0800

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
